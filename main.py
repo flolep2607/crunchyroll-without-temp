d2fac25db0ff0899161bc927899f48ffea34c9e1
flolep2607 <flo.lep2607@gmail.com>
2020-06-23 17:30:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def getter(url):
         print("Cookies miam🍪.. Loaded")
     # session.cookies=cj #when add cookie can't bypass cloudflare
     scraper = cloudscraper.create_scraper(browser='chrome')#sess=session)
-    scraper.cookies=cj#.set_cookie(cookiejar.Cookie(version=0, name='OptanonAlertBoxClosed', value='2020-06-23T11:42:41.657Z', port=None, port_specified=False, domain='.www.crunchyroll.com', domain_specified=True, domain_initial_dot=True, path='/', path_specified=False, secure=False, expires=1624448561, discard=False, comment=None, comment_url=None, rest={}, rfc2109=False))
-    wb_page=scraper.get(url)#"https://www.crunchyroll.com/fr/tower-of-god/episode-11-underwater-hunt-part-one-794529").text
+    scraper.cookies=cj
+    wb_page=scraper.get(url)
     if wb_page.ok:
         if "showmedia-trailer-notice" in wb_page.text:
             raise Exception("Your are not premium")
@@ -70,8 +70,6 @@ def getter(url):
                 print(tmp.data)
                 if tmp.playlists:
                     for arg in tmp.playlists:
-                        # arg=re.search(tmp.text,re.MULTILINE).groupdict()
-                        # print(arg)
                         if not best or best.stream_info.resolution[0]<arg.stream_info.resolution[0]:
                             best=arg
                         elif best.stream_info.resolution[0]==arg.stream_info.resolution[0]:
@@ -111,18 +109,6 @@ def main(infos,outfile,username=None,password=None):
                 for part in input_stream.iter_content(chunk_size=512):
                     output_file.write(cipher_encrypt.decrypt(part))
                 print(progress_bar.update(),end="\r")
-    # elif True:
-    #     URL="https://dl.v.vrv.co/evs/10ac443c0ba37d3d252ef9e4fe819657/assets/10ac443c0ba37d3d252ef9e4fe819657_3831456.mp4/clipFrom/0000/clipTo/120000/index.m3u8?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cCo6Ly9kbC52LnZydi5jby9ldnMvMTBhYzQ0M2MwYmEzN2QzZDI1MmVmOWU0ZmU4MTk2NTcvYXNzZXRzLzEwYWM0NDNjMGJhMzdkM2QyNTJlZjllNGZlODE5NjU3XzM4MzE0NTYubXA0L2NsaXBGcm9tLzAwMDAvY2xpcFRvLzEyMDAwMC9pbmRleC5tM3U4IiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNTkzMDk2MTU0fX19XX0_&Signature=Ej5fX5Zs9OB3Rxm9hCbLDLhl~SDOYS1xa6THOSEfyNqEc25bhewGIFGPY-pGDWNeKAqqVcFx6sG4ZUIl313hML2kXaaiG86OfN0Z9FUqAKXVLra8hH5AFbMKxhzbQaHpU~9Nb7k59MUFjC9Tk653-Iwl1qmVP4hnWq3E2I2ps-KCoTnQj5LY1ZnJm~7Euv6j5mXAZoAJmBlLPZ-mW-U8zJSilWz4n2EfM8u9xvcz3WwWG5FO0WU0Kr3a5XTyK89GkCglEobxIGGXZlf9NoF22SIpk79HbJSoveTm0I1Al-45Va-Sno1Rx0YEy2TEBrQ~JBPsQ3J9aJC60HSfRT7WYQ__&Key-Pair-Id=DLVR"
-    #     r=m3u8.load(URL)
-    #     key=requests.get(r.keys[0].uri).content
-    #     progress_bar=ProgressBar(len(r.segments))
-    #     with open(outfile,"wb") as output_file:
-    #         for fil in r.segments:
-    #             input_stream=requests.get(fil.uri,stream=True) 
-    #             cipher_encrypt = AES.new(key, AES.MODE_CBC, key)
-    #             for part in input_stream.iter_content(chunk_size=512):
-    #                 output_file.write(cipher_encrypt.decrypt(part))
-    #             print(progress_bar.update(),end="\r")
     else:
         print("\033[1m"+"╔"+"═"*40+"╗")
         print("║"+"Humm something happen have you crunchy premium?".ljust(40)+"║")
@@ -132,7 +118,5 @@ def main(infos,outfile,username=None,password=None):
 parser = argparse.ArgumentParser()
 parser.add_argument("l", type=str, help="link")
 parser.add_argument("o", type=str, help="output file")
-parser.add_argument("-u", "--username", type=str, help="username", default=None)#,required=False)
-parser.add_argument("-p", "--password", type=str, help="password", default=None)#,required=False)
 args = parser.parse_args()
-main(getter(args.l),args.o,args.username,args.password)
+main(getter(args.l),args.o)
